ea6854c87ba8bd8bf3630ff11db9840cdd3b4ef5
Andy Castillo <acastillo@t-contacta.com>
2019-09-27 13:25:07 -0500

    agregando modulo formulario parcial

--- src/app/formulario/formulario-content/formulario-content.component.ts
+++ src/app/formulario/formulario-content/formulario-content.component.ts
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormularioContentComponent implements OnInit {
 
+  generoList: any = [
+    { id: 1, valor: 'Masculino' },
+    { id: 2, valor: 'Femenino' }
+  ]
   constructor() { }
 
+  employee: any
+
   ngOnInit() {
+    // document.addEventListener('DOMContentLoaded', function() {
+    //   var elems = document.querySelectorAll('.datepicker');
+    //   var instances = M.Datepicker.init(elems, options);
+    // });
+
   }
+
+
 
 }
