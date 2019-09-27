2a58eb3ce16cc6a2452c8629366db6adf0856b15
Andy Castillo <acastillo@t-contacta.com>
2019-09-27 12:48:19 -0500

    agregando unsubscribe

--- src/app/componentes/componentes-content/employee-page/employee-page.component.ts
+++ src/app/componentes/componentes-content/employee-page/employee-page.component.ts
@@ -1,28 +1,34 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnDestroy } from '@angular/core';
 import { DataService } from 'src/app/services/data.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-employee-page',
   templateUrl: './employee-page.component.html',
   styleUrls: ['./employee-page.component.scss']
 })
-export class EmployeePageComponent implements OnInit {
+export class EmployeePageComponent implements OnInit, OnDestroy {
 
   title = 'Employee';
 
-  employee: any = {    
+  employee: any = {
     detail: {
     }
   };
+  employeeSuscription: Subscription;
 
 
   constructor(public dataService: DataService) { }
 
   ngOnInit() {
-    this.dataService.employee.subscribe(_el => {
+    this.employeeSuscription = this.dataService.employee.subscribe(_el => {
       this.employee = _el;
       console.log(_el)
     })
+  }
+  
+  ngOnDestroy() {
+    this.employeeSuscription.unsubscribe();
   }
 
 }
